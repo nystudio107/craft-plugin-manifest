55f16271b5db05a510f1fa186dc54aa244569ce8
Andrew Welch <andrew@nystudio107.com>
2021-04-07 22:58:11 -0400

    Add a `100ms` delay when requesting the manifest file if using it in hot mode, as a hack to avoid a `webpack-dev-server` / Tailwind CSS JIT race condition (https://github.com/nystudio107/craft/issues/55)
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/services/ManifestService.php
+++ src/services/ManifestService.php
@@ -448,12 +448,20 @@ class ManifestService extends Component
                 $result = null;
                 $contents = null;
                 if (UrlHelper::isAbsoluteUrl($path)) {
-                    $client = new Client([
+                    $clientOptions = [
                         RequestOptions::HTTP_ERRORS => false,
                         RequestOptions::CONNECT_TIMEOUT => 3,
                         RequestOptions::VERIFY => false,
                         RequestOptions::TIMEOUT => 5,
-                    ]);
+                    ];
+                    // If we're hot, insert a short 50ms delay in fetching remove files, to handle a webpack-dev-server/
+                    // Tailwind CSS JIT race condition
+                    if ($this->isHot) {
+                        $clientOptions = array_merge($clientOptions, [
+                            RequestOptions::DELAY => 100,
+                        ]);
+                    }
+                    $client = new Client($clientOptions);
                     try {
                         $response = $client->request('GET', $path, [
                             RequestOptions::HEADERS => [
